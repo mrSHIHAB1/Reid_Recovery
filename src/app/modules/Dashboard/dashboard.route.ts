cad662a11dd3656db2067a033a33f5d6a898411e
mrSHIHAB1 <montasirr36@gmail.com>
2026-02-17 15:27:48 +0600

    adfa

--- src/app/modules/Dashboard/dashboard.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboard } from "./dashboard.controller";
-
-const router = express.Router();
-
-router.get("/dashboard", getDashboard);
-
-export default router;
